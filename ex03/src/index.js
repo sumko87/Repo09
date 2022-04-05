4865d682f80c0aae1b9acff0f39bbc1902e472e9
Sumedin Bilalic <sumko87@gmail.com>
2022-04-05 19:06:03 +0200

    equality operator in JS

---
+++ ex03/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function myEqualFunction(num) {
+    if(num==23) {
+        return "Equal";
+    }
+    return "Not equal";
+}
+console.log(myEqualFunction(23));
+console.log(myEqualFunction(25));
+// Only change code above this line
+module.exports = myEqualFunction;
