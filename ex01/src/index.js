bd5a181104c5630a966436f57757a58541e72727
Sumedin Bilalic <sumko87@gmail.com>
2022-04-05 18:08:29 +0200

    working with booolean in JS

---
+++ ex01/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function myTrueBoolean() {
+return true;
+}
+console.log(myTrueBoolean());
+// Only changec code above this line
+module.exports = myTrueBoolean;
+
